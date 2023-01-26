b64b571f4cd7fd56cf3882c50fdc83d563120f71
Tim Quigg <timjquigg@gmail.com>
2023-01-26 16:32:36 -0700

    Initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { CacheProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import createEmotionCache from "../createEmotionCache";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
 }
